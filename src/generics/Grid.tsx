997b86ecf3597b4e424dc1a4dd9e5f92e08b1680
Lewis <lewisthornton@btinternet.com>
2023-02-21 18:50:16 +0000

    added sharp for image optimisation

--- src/generics/Grid.tsx
+++ src/generics/Grid.tsx
@@ -10,8 +10,8 @@ export default function Grid(props: GridProps) {
   return (
     <div className="p-8 bg-neutral-900">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-8">
-        {props.gridItems.map(aboutItem => (
-          <div className="p-4 bg-white rounded-md flex items-center justify-center">
+        {props.gridItems.map((aboutItem, index) => (
+          <div key={index} className="p-4 bg-white rounded-md flex items-center justify-center">
             {aboutItem}
           </div>
         ))}
